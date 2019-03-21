188a019d8637ce9f4cf4551ce8df4e5c79bffea6
k-soda <>
2019-03-21 23:20:05 +0900

    Add the ZipManager class.

--- UnzipInMemorySample/ViewController.swift
+++ UnzipInMemorySample/ViewController.swift
@@ -7,17 +7,12 @@
 //
 
 import UIKit
-import ZIPFoundation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var backButton: UIBarButtonItem!
     @IBOutlet weak var nextButton: UIBarButtonItem!
-    
-    var archive: Archive?
-    var entries = [Entry]()
-    var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,56 +21,26 @@ class ViewController: UIViewController {
             return
         }
         
-        archive = Archive(url: url, accessMode: .read)
-        
-        guard let archive = archive else {
-            return
-        }
-        
-        let iterator = archive.makeIterator()
-        
-        entries = iterator.map { $0 }
-        
-        if entries.isEmpty {
-            return
-        }
-        
-        do {
-            try archive.extract(entries.first!, consumer: { data in
-                imageView.image = UIImage(data: data)
-            })
-        } catch {
-            debugPrint(error.localizedDescription)
+        ZipManager.shared.read(url: url) { data in
+            self.imageView.image = UIImage(data: data)
         }
     }
     
     @IBAction func didTapBackButton(_ sender: Any) {
-        let newIndex = index - 1
-        guard 0 <= newIndex else {
-            return
+        ZipManager.shared.goBack { data in
+            self.imageView.image = UIImage(data: data)
         }
-        index = newIndex
-        do {
-            try archive?.extract(entries[index], consumer: { data in
-                imageView.image = UIImage(data: data)
-            })
-        } catch {
-            debugPrint(error.localizedDescription)
-        }
+        
+        backButton.isEnabled = ZipManager.shared.canGoBack()
+        nextButton.isEnabled = ZipManager.shared.canGoNext()
     }
     
     @IBAction func didTapNextButton(_ sender: Any) {
-        let newIndex = index + 1
-        guard newIndex < entries.count else {
-            return
+        ZipManager.shared.goNext { data in
+            self.imageView.image = UIImage(data: data)
         }
-        index = newIndex
-        do {
-            try archive?.extract(entries[index], consumer: { data in
-                imageView.image = UIImage(data: data)
-            })
-        } catch {
-            debugPrint(error.localizedDescription)
-        }
+        
+        backButton.isEnabled = ZipManager.shared.canGoBack()
+        nextButton.isEnabled = ZipManager.shared.canGoNext()
     }
 }
---
+++ UnzipInMemorySample/ZipManager.swift
@@ -0,0 +1,110 @@
+//
+//  ZipManager.swift
+//  UnzipInMemorySample
+//
+//  Created by KS on 2019/03/21.
+//  Copyright © 2019 KS. All rights reserved.
+//
+
+import Foundation
+import ZIPFoundation
+
+class ZipManager {
+    
+    enum Result {
+        case success
+        case failure
+    }
+    
+    typealias Process = (Data) -> Void
+    
+    static let shared = ZipManager()
+    
+    private var archive: Archive?
+    private var entries = [Entry]()
+    private var index = 0
+    
+    @discardableResult
+    func read(url: URL, process: Process? = nil) -> Result {
+        archive = Archive(url: url, accessMode: .read)
+        
+        guard let archive = archive else {
+            return .failure
+        }
+        
+        let iterator = archive.makeIterator()
+        
+        entries = iterator.map { $0 }
+        
+        if entries.isEmpty {
+            return .failure
+        }
+        
+        index = 0
+        return goTo(index: index, process: process)
+    }
+    
+    func canGoBack() -> Bool {
+        guard 0 <= index - 1 else {
+            return false
+        }
+        
+        return true
+    }
+    
+    func canGoNext() -> Bool {
+        guard index + 1 < entries.count else {
+            return false
+        }
+        
+        return true
+    }
+    
+    @discardableResult
+    func goBack(process: Process? = nil) -> Result {
+        guard canGoBack() else {
+            return .failure
+        }
+        
+        index -= 1
+        return goTo(index: index, process: process)
+    }
+    
+    @discardableResult
+    func goNext(process: Process? = nil) -> Result {
+        guard canGoNext() else {
+            return .failure
+        }
+        
+        index += 1
+        return goTo(index: index, process: process)
+    }
+    
+    @discardableResult
+    func goFirst(process: Process? = nil) -> Result {
+        index = 0
+        return goTo(index: index, process: process)
+    }
+    
+    @discardableResult
+    func goLast(process: Process? = nil) -> Result {
+        index = entries.count - 1
+        return goTo(index: index, process: process)
+    }
+}
+
+private extension ZipManager {
+    
+    func goTo(index: Int, process: Process? = nil) -> Result {
+        do {
+            _ = try archive?.extract(entries[index], consumer: { data in
+                process?(data)
+            })
+        } catch {
+            debugPrint(error.localizedDescription)
+            return .failure
+        }
+        
+        return .success
+    }
+}
